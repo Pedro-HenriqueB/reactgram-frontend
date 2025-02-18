061eb09db149ce2c6a3ef69cf8264ca294a15965
Pedro Benegas <pedrophbs8199@gmail.com>
2025-02-17 21:16:44 -0300

    add: home, login, register routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+// import css
 import "./App.css";
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
     <>
-      <h1>ReactGram</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
