3712ac80cef6177761eb47bf6aa8e728d4d9c4f7
Pedro Benegas <pedrophbs8199@gmail.com>
2025-02-17 22:37:07 -0300

    add: routes basic html and styles

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,7 +1,28 @@
 import "./Auth.css";
+// Components
+import { Link } from "react-router-dom";
+// Hooks
+import { useState, useEffect } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para viualizar fotos incriveis!</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
