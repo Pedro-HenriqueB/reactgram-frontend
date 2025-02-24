eb03fda106de578870b4417a52046fc76be6cd2e
Pedro Benegas <pedrophbs8199@gmail.com>
2025-02-24 20:27:26 -0300

    add: auth hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import "./App.css";
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// Hooks
+import { useAuth } from "./hooks/useAuth";
+// Components
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
@@ -9,15 +12,29 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
+  const { auth, loading } = useAuth();
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!auth ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
         <Footer />
